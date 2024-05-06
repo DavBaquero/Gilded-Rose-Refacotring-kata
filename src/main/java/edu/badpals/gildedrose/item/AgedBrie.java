ca8536a5d306f26715be48138b2f974f54ee68ce
DavBaquero <144956230+DavBaquero@users.noreply.github.com>
2024-05-06 17:23:28 +0200

    🔧 feat(AgedBrie.java) : add new function to class

--- src/main/java/edu/badpals/gildedrose/item/AgedBrie.java
+++ src/main/java/edu/badpals/gildedrose/item/AgedBrie.java
@@ -7,12 +7,36 @@ public class AgedBrie extends Item{
     }
 
     @Override
-    public void update_sellIn() {
+    public int getQuality() {
+        return super.getQuality();
+    }
 
+    @Override
+    public int getSellIn() {
+        return super.getSellIn();
+    }
+
+    @Override
+    public void setQuality(int quality) {
+        super.setQuality(quality);
+    }
+
+    @Override
+    public void setSellIn(int sellIn) {
+        super.setSellIn(sellIn);
+    }
+
+    @Override
+    public void update_sellIn() {
+        this.setSellIn(this.getSellIn() - 1);
     }
 
     @Override
     public void update_quality() {
-
+        this.update_sellIn();
+        if (getSellIn() > 0)
+            this.setQuality(getQuality() + 1);
+        else
+            this.setQuality(getQuality() + 2);
     }
 }
